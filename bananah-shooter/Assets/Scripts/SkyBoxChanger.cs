ae0da878181f21d05f95546d591c5a42c1ba7548
Markus Pichler <mapichler2001@gmail.com>
2023-05-30 15:35:57 +0200

    directional light also changes

--- bananah-shooter/Assets/Scripts/SkyBoxChanger.cs
+++ bananah-shooter/Assets/Scripts/SkyBoxChanger.cs
@@ -1,17 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEditor;
 
 public class SkyBoxChanger : MonoBehaviour
 {
-    public Material[] skyboxes; // Array of skybox materials
+    [System.Serializable]
+    public struct SkyboxData
+    {
+        public Material skyboxMaterial;
+        public LightingSettings lightingSettings;
+        public GameObject directionalLight;
+    }
+
+    public SkyboxData[] skyboxes;
     private int currentIndex = 0; // Current index of the skybox
+    private GameObject currentDirectionalLight;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        RenderSettings.skybox = skyboxes[currentIndex]; // Set the initial skybox
+        // Set first Skybox in Array
+        SetSkyboxAndLighting(currentIndex);
     }
+
 
     // Update is called once per frame
     void Update()
@@ -23,9 +37,35 @@ public class SkyBoxChanger : MonoBehaviour
         }
     }
 
+
     public void ChangeSkybox()
     {
-        currentIndex = (currentIndex + 1) % skyboxes.Length; // Increment the index and wrap around
-        RenderSettings.skybox = skyboxes[currentIndex]; // Change the skybox to the new material
+        // Increment the index and wrap around
+        currentIndex = (currentIndex + 1) % skyboxes.Length;
+        
+        SetSkyboxAndLighting(currentIndex);
+    }
+
+    void SetSkyboxAndLighting(int index)
+    {
+        // Change the skybox material
+        RenderSettings.skybox = skyboxes[index].skyboxMaterial;
+        //Change lighting Settings
+        Lightmapping.lightingSettings = skyboxes[index].lightingSettings;
+
+        // Change Directional Light
+        GameObject newDirectionalLight = skyboxes[index].directionalLight;
+
+        if (currentDirectionalLight != null)
+        {
+            Destroy(currentDirectionalLight);
+        }
+
+        if (newDirectionalLight != null)
+        {
+            currentDirectionalLight = Instantiate(newDirectionalLight);
+            currentDirectionalLight.transform.position = newDirectionalLight.transform.position;
+            currentDirectionalLight.transform.rotation = newDirectionalLight.transform.rotation;
+        }
     }
 }
