dc9ee922376702671e04778c9bbbc1c96b72785d
Markus Pichler <mapichler2001@gmail.com>
2023-06-01 12:17:29 +0200

    you can ring the gong using the "f" key

--- bananah-shooter/Assets/Scripts/Gong.cs
+++ bananah-shooter/Assets/Scripts/Gong.cs
@@ -2,17 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Gong : MonoBehaviour
+public class Gong : MonoBehaviour, Interactible
 {
+    public bool canInteract;
+    public Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //canInteract = false;
+        canInteract = true;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Interact()
     {
-        
+        canInteract = false;
+        animator.SetTrigger("Ring");
     }
+
+    public bool CanInteract()
+    {
+        return canInteract;
+    }
+
+
 }
--- bananah-shooter/Assets/Scripts/InteractionDetector.cs
+++ bananah-shooter/Assets/Scripts/InteractionDetector.cs
@@ -2,17 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//this script also works when there are more interactible objects next to each other, but right now we only have one
 public class InteractionDetector : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private List<Interactible> interactiblesInRange = new List<Interactible>();
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.F) && interactiblesInRange.Count > 0)
+        {
+            var interactible = interactiblesInRange[0];
+            interactible.Interact();
+
+            if(!interactible.CanInteract())
+            {
+                interactiblesInRange.Remove(interactible);
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // checks if an interactible object is in range and adds it to the list
+        var interactible = other.GetComponent<Interactible>();
+        if(interactible != null && interactible.CanInteract())
+        {
+            interactiblesInRange.Add(interactible);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        // removes the object from the list if it is not in range anymore
+        var interactible = other.GetComponent<Interactible>();
+        if(interactiblesInRange.Contains(interactible))
+        {
+            interactiblesInRange.Remove(interactible);
+        }
+
     }
 }
